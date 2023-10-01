a26b1ceb3eda40cf700716712759bcd18ddc057c
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2023-10-02 01:56:11 +0300

    Update SimilarMovie.vue

--- src/components/Content/SimilarMovie.vue
+++ src/components/Content/SimilarMovie.vue
@@ -39,26 +39,8 @@ export default {
     },
   },
   watch: {
-    "$route"() {
-      const options = {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjJiNWM3YWMyMDZmNGJhMWY1NjI1ZTE0MzNjZWY0MiIsInN1YiI6IjY1MDU5YTAwM2NkMTJjMDE0ZWJlOTA2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SRoNKxC_8eeVrlNIY2IrHDmc6RFfYz8vR5UvBot5kUg",
-        },
-      };
-
-      fetch(
-        `https://api.themoviedb.org/3/movie/${this.$route.params.id}/similar?language=en-US&page=1`,
-        options
-      )
-        .then((response) => response.json())
-        .then((response) => {
-          this.movies = response.results;
-          console.log("SimilarMovie", response.results);
-        })
-        .catch((err) => console.error(err));
+    $route() {
+      this.getSimilar()
       //location.reload();
     },
   },
